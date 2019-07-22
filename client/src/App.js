a111110048730ebaf01093e5020e40788c6650e6
Tgleason1980 <terryscoding@gmail.com>
2019-07-22 18:19:41 -0500

    did routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
-
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+import { Link } from 'react-router-dom';
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
@@ -12,6 +14,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
+      <Link to='/movies/{id}'> <Route exact path="/" component={MovieList} /></Link>
+      <Route exact path="/movies/:id" component={Movie} />
       <div>Replace this Div with your Routes</div>
     </div>
   );
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Link } from 'react-router-dom';
 const MovieList = props => {
   const [movies, setMovies] = useState([])
   useEffect(() => {
@@ -9,6 +9,7 @@ const MovieList = props => {
         .get('http://localhost:5000/api/movies')
         .then(response => {
           setMovies(response.data);
+          console.log(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -23,6 +24,7 @@ const MovieList = props => {
       {movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
+      
     </div>
   );
 }
@@ -39,7 +41,7 @@ function MovieDetails({ movie }) {
         Metascore: <strong>{metascore}</strong>
       </div>
       <h3>Actors</h3>
-
+{/* <Link to='/movies/{id}'></Link> */}
       {stars.map(star => (
         <div key={star} className="movie-star">
           {star}
